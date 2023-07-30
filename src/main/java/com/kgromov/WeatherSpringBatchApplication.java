ac20dc78be374f2b90c53d0852eb13c7abe70799
Konstantin <konstantin@revimedia.com>
2023-07-30 23:52:33 +0300

    Add dummy scheduler

--- src/main/java/com/kgromov/WeatherSpringBatchApplication.java
+++ src/main/java/com/kgromov/WeatherSpringBatchApplication.java
@@ -1,36 +1,15 @@
 package com.kgromov;
 
-import com.kgromov.repository.DailyTemperatureRepository;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableBatchProcessing
+@EnableScheduling
 public class WeatherSpringBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherSpringBatchApplication.class, args);
     }
 
-    @Bean
-    ApplicationRunner applicationRunner(JobLauncher jobLauncher,
-                                        Job syncTemperatureJob,
-                                        DailyTemperatureRepository temperatureRepository) {
-        return args -> {
-            LocalDate syncDate = temperatureRepository.getLatestDateTemperature();
-            JobParameters jobParameters = new JobParametersBuilder()
-                    .addLocalDate("syncStartDate", syncDate)
-                    .addLong("startedAt", System.currentTimeMillis())
-                    .toJobParameters();
-            jobLauncher.run(syncTemperatureJob, jobParameters);
-        };
-    }
 }
---
+++ src/main/java/com/kgromov/service/SyncTemperatureScheduler.java
@@ -0,0 +1,34 @@
+package com.kgromov.service;
+
+import com.kgromov.repository.DailyTemperatureRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SyncTemperatureScheduler {
+    private final JobLauncher jobLauncher;
+    private final DailyTemperatureRepository temperatureRepository;
+    private final Job syncTemperatureJob;
+
+    @Scheduled(cron = "0 0/5 * * *")
+    public void perform() throws Exception {
+        log.info("Schedule sync temperature job ...");
+        LocalDate syncDate = temperatureRepository.getLatestDateTemperature();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLocalDate("syncStartDate", syncDate)
+                .addLong("startedAt", System.currentTimeMillis())
+                .toJobParameters();
+        jobLauncher.run(syncTemperatureJob, jobParameters);
+        log.info("Finish sync temperature job ...");
+    }
+}
